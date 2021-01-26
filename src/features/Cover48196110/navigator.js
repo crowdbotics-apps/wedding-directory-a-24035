9558674bab92b83078a09cb250ae3820182a0d3f
Crowdbotics <team@crowdbotics.com>
2021-01-26 11:09:57 +0000

    remove package blueprint-react-native-blank-screen

--- src/features/Cover48196110/navigator.js
+++
@@ -1,6 +0,0 @@
-import { createStackNavigator } from "react-navigation-stack"
-import Blank from "./screens/index"
-export default BlankNavigator = createStackNavigator(
-  { Blank: { screen: Blank } },
-  { initialRouteName: "Blank" }
-)
