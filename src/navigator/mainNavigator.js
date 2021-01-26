ac1e7fbf34f0ec6f9cf302554fbdda46c34f436b
Crowdbotics <team@crowdbotics.com>
2021-01-26 11:18:00 +0000

    remove package blueprint-react-native-achievement-screen

--- src/navigator/mainNavigator.js
+++ src/navigator/mainNavigator.js
@@ -7,7 +7,6 @@ import SideMenu from './sideMenu';
 //@BlueprintImportInsertion
 import BlankScreen2197000Navigator from '../features/BlankScreen2197000/navigator';
 import Achievements1196999Navigator from '../features/Achievements1196999/navigator';
-import Achievements2196998Navigator from '../features/Achievements2196998/navigator';
 import BlankScreen24196997Navigator from '../features/BlankScreen24196997/navigator';
 import BlankScreen23196606Navigator from '../features/BlankScreen23196606/navigator';
 
@@ -20,7 +19,6 @@ const AppNavigator = {
     //@BlueprintNavigationInsertion
 BlankScreen2197000: { screen: BlankScreen2197000Navigator },
 Achievements1196999: { screen: Achievements1196999Navigator },
-Achievements2196998: { screen: Achievements2196998Navigator },
 BlankScreen24196997: { screen: BlankScreen24196997Navigator },
 BlankScreen23196606: { screen: BlankScreen23196606Navigator },
 
